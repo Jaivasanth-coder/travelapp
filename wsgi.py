bf3c00bb20cbcb41939592b0f55b792af3a25da9
Jaivasanth <jaivasanth78@gmail.com>
2026-03-18 11:57:13 +0530

    wsgi changes

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,10 @@
 # wsgi.py
+import sys
+import os
+
+# Add the backend folder to Python's path so 'app' can be found
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'backend'))
+
 from app import app, init_db, seed_sample_data
 
 init_db()
